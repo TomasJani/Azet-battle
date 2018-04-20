91b24f30451f1832eeeeafcd448ef438e216a463
cucak559 <michalcaniga559@gmail.com>
2018-04-20 10:45:45 +0200

    Another views

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -3,7 +3,23 @@
 @section("content")
 	<div class="container">
 		<div class="row">
+			<div class="question col-md-10">
+				<h2 class="heading">{{auth()->user()->company->name}}</h2>
+				<hr>
 
+				@foreach($questions as $question)
+					<a href="/{{$question->company_id}}/question/{{$question->id}}" title="">{{$question->name}}</a>
+					<div class="questionBody">
+					 	{{ $question->body }}
+					</div>
+
+					<div>
+							<p><small>{{$question->created_at}}</small></p>
+							<p><small>{{$question->user->name}}</small></p>
+					</div>
+
+				@endforeach
+			</div>
 		</div>
 	</div>
 @endsection
