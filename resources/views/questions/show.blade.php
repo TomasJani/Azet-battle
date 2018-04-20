463486b88d76fc3ea4f16021ef7da0f514c4cbd2
Tomas <tomasjanicek221@gmail.com>
2018-04-20 17:08:01 +0200

    Show styles

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -2,99 +2,86 @@
 
 @section("content")
 	<div class="container">
+
 		<div class="row">
-			<div class="questionarea col-md-10">
-				<h2 class="heading">{{auth()->user()->company->name}}</h2>
+			<div class="questionarea col-md-12">
+				<div class="companyName">
+				 	<h2>{{ $question->name }}</h2>
+				</div>
 				<hr>
-
 				<div class="row">
-
 					<div class="col-md-1 rating">
-							<div class="like">
-								<form action="/{{auth()->user()->company->id}}/question/{{$question->id}}/like" method="POST">
-									{{csrf_field()}}
-									{{method_field("PATCH")}}
 
-									<button class="btn btn-primary" type="submit">
-										<i class="fas fa-arrow-up"></i>
-									</button>
+						<div class="like">
+							<form action="/{{auth()->user()->company->id}}/question/{{$question->id}}/like" method="POST">
+								{{csrf_field()}}
+								{{method_field("PATCH")}}
+								<button class="btn btn-primary" type="submit">
+									<i class="fas fa-arrow-up"></i>
+								</button>
+							</form>
+						</div>
 
-								</form>
+						<div class="r text-center">
+							{{ $question->rating }}
+						</div>
 
+						<div class="dislike">
+							<form action="/{{auth()->user()->company->id}}/question/{{$question->id}}/dislike" method="POST">
+								{{csrf_field()}}
+								{{method_field("PATCH")}}
 
-							</div>
+								<button type="submit" class="btn btn-primary">
+									<i class="fas fa-arrow-down"></i>
+								</button>
 
-							<div class="r">
-								{{ $question->rating }}
-							</div>
-
-							<div class="dislike">
-								<form action="/{{auth()->user()->company->id}}/question/{{$question->id}}/dislike" method="POST">
-									{{csrf_field()}}
-									{{method_field("PATCH")}}
-
-									<button type="submit" class="btn btn-primary">
-										<i class="fas fa-arrow-down"></i>
-									</button>
-
-								</form>
-							</div>
+							</form>
+						</div>
 					</div>
 
 					<div class="col-md-9 question">
 
-						<div class="questionName">
-						 	<h4>{{ $question->name }}</h4>
-						</div>
-
 						<div class="">
-						 	<?= $question->body ?>
+						 	{{-- <?= $question->body ?> --}}
 						</div>
 
-						<div>
+						<div class="ml-auto">
 								<p><small>{{$question->created_at}}</small></p>
 								<p><small>{{$question->user->name}}</small></p>
 						</div>
 
 					</div>
 
-					<div class="col-md-2 controls">
-						<div class="row">
-							<div class="col-md-6">
-								<form action="/{{auth()->user()->company->id}}/question/{{ $question->id }}/edit" method="GET">
-									{{csrf_field()}}
-									<button type="submit" class="btn btn-primary">Edit</button>
-								</form>
-							</div>
+					<div class="col-md-2 controls content-justify-content-center">
+						<form action="/{{auth()->user()->company->id}}/question/{{ $question->id }}/edit" method="GET">
+							{{csrf_field()}}
+							<button type="submit" class="btn btn-primary">Edit</button>
+						</form>
 
-							<div class="col-md-6">
-								<form action="/{{auth()->user()->company->id}}/question/{{ $question->id }}/delete" method="POST">
-									{{ csrf_field() }}
-									{{ method_field("DELETE") }}
-									<button type="submit" class="btn btn-danger">Delete</button>
-								</form>
-							</div>
-
-						</div>
+						<form action="/{{auth()->user()->company->id}}/question/{{ $question->id }}/delete" method="POST">
+							{{ csrf_field() }}
+							{{ method_field("DELETE") }}
+							<button type="submit" class="btn btn-danger">Delete</button>
+						</form>
 					</div>
-
 				</div>
-
 			</div>
 		</div>
 
-		<div class="row">
-			<div class="comments col-md-12">
-				@foreach ($question->comments as $comment)
-					<div class="card mb-1 text-white bg-secondary">
-					  <div class="card-body">
-					    <p class="card-text">{{$comment->body}}</p>
-					    <p class="card-text"> by {{$comment->user->name}} at {{$comment->created_at}}</p>
-					  </div>
-					</div>
-				@endforeach
-			</div>
+	<hr>
+
+	<div class="row">
+		<div class="comments col-md-9 push-2">
+			@foreach ($question->comments as $comment)
+				<div class="card mb-1 text-white bg-secondary">
+				  <div class="card-body">
+				    <p class="card-text">{{$comment->body}}</p>
+				    <p class="card-text"> by {{$comment->user->name}} at {{$comment->created_at}}</p>
+				  </div>
+				</div>
+			@endforeach
 		</div>
+	</div>
 
 		<hr>
 
